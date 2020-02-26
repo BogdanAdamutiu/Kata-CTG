48148cef946322ce7b9bb1c6b8b818c7b489688b
Bogdan <bogdan.adamutiu@gmail.com>
2020-02-26 17:41:49 +0100

    Added class cards

--- player.test.js
+++ player.test.js
@@ -2,7 +2,21 @@
 
 const instanceOfPlayer = require('./Player/factory.player');
 
-it('first test', () => {
-    const playerOne = instanceOfPlayer();
-    expect(playerOne.getHealth()).toEqual(30);
+it('Scenario: player is instantiated and has 30 health', () => {
+    const player = instanceOfPlayer();
+    expect(player.getHealth()).toEqual(30);
 })
+
+it('Scenario: player suffers 1 damage and has a health of 1 less than starting health', () => {
+    const player = instanceOfPlayer();
+    const startingHealthOfPlayer = player.getHealth();
+    player.sustainDamage(1);
+    expect(player.getHealth()).toStrictEqual(startingHealthOfPlayer - 1);
+});
+
+it('Scenario: player heals 1 damage and has a health of 1 more than starting health', () => {
+    const player = instanceOfPlayer();
+    const startingHealthOfPlayer = player.getHealth();
+    player.healDamage(1);
+    expect(player.getHealth()).toStrictEqual(startingHealthOfPlayer + 1);
+});
